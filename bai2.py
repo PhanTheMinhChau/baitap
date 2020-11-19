4a6c664e8f9f5a47fc414edf16555ed72bed3637
Phan Thế Minh Châu <73175450+PhanTheMinhChau@users.noreply.github.com>
2020-11-19 22:35:55 +0700

    Add files via upload

---
+++ bai2.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 19 21:33:54 2020
+
+@author: PHAN FAMILY
+"""
+
+print("giải phương trình ax+b=0")
+n = int(input("số lần lặp: "))
+i = 1
+while i <= n :
+    a = float(input("nhập số a "))
+    b = float(input("nhập số b "))
+    if a==0 and b==0:
+        print("phương trình vô số nghiệm")
+    if a==0 and b!=0:
+        print("phương trình vô nghiệm")
+    if a!=0:
+        x = -b/a
+        print("nghiệp của phương trình",a,"x+",b,"=0 là", x )
+    i = i + 1
+    
+print("kết thúc")
+# vòng lặp for
+n=int(input("số lần lặp: "))
+for i in range(n):
+    a = float(input("nhập số a "))
+    b = float(input("nhập số b "))
+    if a==0 and b==0:
+        print("phương trình vô số nghiệm")
+    if a==0 and b!=0:
+        print("phương trình vô nghiệm")
+    if a!=0:
+        x = -b/a
+        print("nghiệp của phương trình",a,"x+",b,"=0 là", x )
+    
+print("kết thúc")
