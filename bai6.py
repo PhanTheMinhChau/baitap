df292cea9ac76c6b93c908e2b42b28a24a4ef550
Phan Thế Minh Châu <73175450+PhanTheMinhChau@users.noreply.github.com>
2020-11-20 15:42:15 +0700

    Add files via upload

---
+++ bai6.py
@@ -0,0 +1,10 @@
+import random
+a = []
+n = int(input("nhập n "))
+for i in range(n): #tạo list n phần tử
+    a.append(random.random())
+m = 0
+for i in range(n): #giá trị lớn nhất
+    if a[i] > m:
+        m = a[i]
+print("giá trị lớn nhất trong ", a, "là:",m)
