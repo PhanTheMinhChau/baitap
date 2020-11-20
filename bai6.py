4a79461d486ea9ec1b8be9dc409d3024be2fe994
Phan Thế Minh Châu <73175450+PhanTheMinhChau@users.noreply.github.com>
2020-11-20 16:05:04 +0700

    Update bai6.py

--- bai6.py
+++ bai6.py
@@ -1,9 +1,10 @@
 import random
+import math
 a = []
 n = int(input("nhập n "))
 for i in range(n): #tạo list n phần tử
     a.append(random.random())
-m = 0
+m = - math.inf
 for i in range(n): #giá trị lớn nhất
     if a[i] > m:
         m = a[i]
