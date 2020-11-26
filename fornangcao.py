4efee3b1a2d3abbaec4185b2c8e98e437d500018
Phan Thế Minh Châu <73175450+PhanTheMinhChau@users.noreply.github.com>
2020-11-26 08:25:37 +0700

    Add files via upload

---
+++ fornangcao.py
@@ -0,0 +1,8 @@
+ list2 = [({'name': 'Peter', 'age':2}, {'name': 'John', 'age':21}), 
+         ({'name': 'Mary', 'age':2}, {'name': 'Trandanpro', 'age':21}), 
+         ({'name': 'Nam', 'age':2}, {'name': 'Hung', 'age':21}), 
+         ({'name': 'Mai', 'age':2}, {'name': 'Loan', 'age':21})]
+n=len(list2)
+for i in range(n):
+    for ii in enumerate(list2[i]):
+     print(ii)
