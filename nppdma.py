31ced38ee4a5a20c9bb0c7a21bb9bf1e474fa244
Phan Thế Minh Châu <73175450+PhanTheMinhChau@users.noreply.github.com>
2020-11-29 09:17:18 +0700

    Add files via upload

---
+++ nppdma.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as mp
+#ma trận-----------------------------------------------------------------------
+a = np.array([[1, 6,7],[87,5,9],[7,9,0]])
+b = np.array([[2,6,7],[8,23,6],[-3,7,12]])
+print("tổng hai ma trận là: ")
+print(a+b) #tổng hai ma trận
+print("")
+print("tích hai ma trận là: ")
+print(a*b) #tích hai ma trận
+print("chuyển vị ma trận a là: ")
+print(a.transpose())      #chuyển vị ma trận
+print("chuyển vị ma trận b là: ")
+print(b.transpose())      #chuyển vị ma trận
+#đọc file----------------------------------------------------------------------
+#đọc file csv từ máy tính
+n = pd.read_csv("test.csv")
+n = n.head(10)
+print('file từ máy')
+print(n)
+#đọc file csv từ internet
+data = pd.read_csv('https://phantheminhchau.github.io/test/test.csv')
+data = data.head(10)
+print('file từ internet')
+print(data)
+#đọc file JSON từ internet
+js = pd.read_json('https://phantheminhchau.github.io/test/JSON.json', orient='records')
+print("file JSON từ internet")
+for i in range(10):
+    print(js[i])
+#đồ thị------------------------------------------------------------------------
+name = ["A",'B','C','D']
+gt = [70,82,50,60]
+
+mp.barh(name, gt, color='blue')
+mp.title("đồ thị hình bar")
+mp.xlabel("giá trị")
+mp.ylabel("name")
+mp.show()
+
+mp.bar(name, gt, color='green')
+mp.title("đồ thị hình column")
+mp.xlabel("name")
+mp.ylabel("giá trị")
+mp.show()
+
+mp.plot(name, gt, color='red')
+mp.title("đồ thị hình line")
+mp.xlabel("name")
+mp.ylabel("giá trị")
+mp.show()
+
+mp.boxplot(gt)
+mp.title("đồ thị hình boxplot")
+mp.xlabel("name")
+mp.ylabel("giá trị")
+mp.show()
