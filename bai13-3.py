ca747888cf32ad1bb0177970aa4f9684a7f4e644
Phan Thế Minh Châu <73175450+PhanTheMinhChau@users.noreply.github.com>
2020-12-10 17:02:59 +0700

    spyder

---
+++ bai13-3.py
@@ -0,0 +1,17 @@
+import os, random
+add = os.getcwd()
+a = float(input("nhập dung lượng giới hạn(1-1024MB): "))*1024
+while ((a/1024)<1) or ((a/1024)>1024):
+    a = float(input("yêu cầu nhập lại (1-1024MB)"))*1024
+n = int(a//1000) #số file
+c = (a%1000)*1024 #dung lượng file cuối(byte)
+os.mkdir("thư mục chứa file")
+for i in range(n):
+    os.chdir(add+"\\thư mục chứa file")
+    f = open("file"+str(i+1), "x")
+    f.write(''.join(random.choice('0123456789abcdefghijklmnopqrstuvwxyz ') for i in range(1024000)))
+if c!=0:
+    os.chdir(add+"\\thư mục chứa file")
+    f = open("file"+str(i+2), "x")
+    f.write(''.join(random.choice('0123456789abcdefghijklmnopqrstuvwxyz ') for i in range(1024000)))
+print("kết thúc")
